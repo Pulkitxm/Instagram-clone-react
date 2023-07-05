6634241ab696f64cb28cdefd1ef35cd27c8112b9
Pulkit <kpulkit15234@gmail.com>
2023-07-05 11:37:52 +0530

    starter files

---
+++ src/App.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Header from './components/Header.js';
+import Sidebar from './components/Sidebar.js';
+import Home from './components/Home.js';
+import Search from './components/Search.js';
+import Explore from './components/Explore.js';
+import Reels from './components/Reels.js';
+import Messages from './components/Messages.js';
+import Notifications from './components/Notifications.js';
+import Profile from './components/Profile.js';
+
+export default ()=> {
+  return (
+    <Router>
+      <div className="app">
+        <Sidebar />
+        <div className="app__content">
+          <Header />
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/search" element={<Search />} />
+            <Route path="/explore" element={<Explore />} />
+            <Route path="/reels" element={<Reels />} />
+            <Route path="/messages" element={<Messages />} />
+            <Route path="/notifications" element={<Notifications />} />
+            <Route path="/profile" element={<Profile />} />
+          </Routes>
+        </div>
+      </div>
+    </Router>
+  );
+}
